76075d4e172611a68f72509fd5d1870bacf5410e
Rob Kennedy <rob@kennedyprojects.com>
2024-08-28 00:31:32 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+$host = '<host ip>';
+$db   = 'sports_bets';
+$user = 'sports_user';
+$pass = '<passwd>';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
